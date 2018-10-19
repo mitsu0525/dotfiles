1a1d117ba6927afcedb96493c3d91347d0cd5374
k-mituys <k-mituys@ist.osaka-u.ac.jp>
2018-10-19 17:36:14 +0900

    Use Emacs mode

--- .zsh/20_keybinds.zsh
+++ .zsh/20_keybinds.zsh
@@ -1,67 +1,9 @@
-# Vim-like keybind as default
-bindkey -v
-# Vim-like escaping jj keybind
-bindkey -M viins 'jj' vi-cmd-mode
+# emacs keybinds
+bindkey -e
 
-# Add emacs-like keybind to viins mode
-bindkey -M viins '^F'  forward-char
-bindkey -M viins '^B'  backward-char
-bindkey -M viins '^A'  beginning-of-line
-bindkey -M viins '^E'  end-of-line
-bindkey -M viins '^K'  kill-line
-bindkey -M viins '^Y'  yank
-bindkey -M viins '^W'  backward-kill-word
-bindkey -M viins '^U'  backward-kill-line
-bindkey -M viins '^H'  backward-delete-char
-bindkey -M viins '^?'  backward-delete-char
-bindkey -M viins '^G'  send-break
-bindkey -M viins '^D'  delete-char-or-list
-
-bindkey -M vicmd '^A'  beginning-of-line
-bindkey -M vicmd '^E'  end-of-line
-bindkey -M vicmd '^K'  kill-line
-bindkey -M vicmd '^P'  up-line-or-history
-bindkey -M vicmd '^N'  down-line-or-history
-bindkey -M vicmd '^Y'  yank
-bindkey -M vicmd '^W'  backward-kill-word
-bindkey -M vicmd '^U'  backward-kill-line
-bindkey -M vicmd '/'   vi-history-search-forward
-bindkey -M vicmd '?'   vi-history-search-backward
-
-bindkey -M vicmd 'gg' beginning-of-line
-bindkey -M vicmd 'G'  end-of-line
-
-if is-at-least 5.0.8; then
-    autoload -Uz surround
-    zle -N delete-surround surround
-    zle -N change-surround surround
-    zle -N add-surround surround
-    bindkey -a cs change-surround
-    bindkey -a ds delete-surround
-    bindkey -a ys add-surround
-    bindkey -a S add-surround
-fi
-
-# Insert a last word
-zle -N insert-last-word smart-insert-last-word
-zstyle :insert-last-word match '*([^[:space:]][[:alpha:]/\\]|[[:alpha:]/\\][^[:space:]])*'
-bindkey -M viins '^]' insert-last-word
-
-# Surround a forward word by single quote
-quote-previous-word-in-single() {
-    modify-current-argument '${(qq)${(Q)ARG}}'
-    zle vi-forward-blank-word
-}
-zle -N quote-previous-word-in-single
-bindkey -M viins '^Q' quote-previous-word-in-single
-
-# Surround a forward word by double quote
-quote-previous-word-in-double() {
-    modify-current-argument '${(qqq)${(Q)ARG}}'
-    zle vi-forward-blank-word
-}
-zle -N quote-previous-word-in-double
-bindkey -M viins '^Xq' quote-previous-word-in-double
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+bindkey "^u" backward-kill-line
 
 #
 # functions
--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -5,8 +5,6 @@ zplug "zplug/zplug", hook-build:'zplug --self-manage'
 
 zplug "~/.zsh", from:local, use:"<->_*.zsh"
 
-zplug "b4b4r07/zsh-vimode-visual", use:zsh-vimode-visual.zsh, defer:3
-
 zplug "b4b4r07/enhancd", use:init.sh
 if zplug check "b4b4r07/enhancd"; then
 	export ENHANCD_FILTER="fzf --height 40% --reverse --ansi"
@@ -32,7 +30,7 @@ zplug "peco/peco", \
 #     as:command, \
 #     rename-to:fzy, \
 #     hook-build:"make && sudo make install"
- 
+
 # # Additional completion definitions for Zsh
 # zplug "zsh-users/zsh-completions"
 
@@ -40,8 +38,8 @@ zplug "peco/peco", \
 # excuting compinit command and sourcing other plugins.
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
-# # ZSH port of Fish shell's history search feature
-# zplug "zsh-users/zsh-history-substring-search"
+# ZSH port of Fish shell's history search feature
+zplug "zsh-users/zsh-history-substring-search"
 
 # This plugin adds many useful aliases and functions.
 zplug "plugins/git", from:oh-my-zsh
